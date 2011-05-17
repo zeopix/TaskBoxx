a1bc101892c31d75c812bcd469c78a3dfc10fc76
Timo Haberkern <Timo.Haberkern@shift-up.de>
2011-05-17 12:34:57 +0200

    Implementing Translation for the GUI

--- src/ShiftUp/TaskBoxxBundle/Controller/SecurityController.php
+++ src/ShiftUp/TaskBoxxBundle/Controller/SecurityController.php
@@ -10,6 +10,7 @@ class SecurityController extends Controller
 
     public function securityCheckAction()
     {
+        echo "bla";
         // The security layer will intercept this request
     }
 
--- src/ShiftUp/TaskBoxxBundle/Entity/User.php
+++ src/ShiftUp/TaskBoxxBundle/Entity/User.php
@@ -1,43 +1,40 @@
-<?php 
-namespace ShiftUp\TaskBoxxBundle\Entity; 
+<?php
+
+namespace ShiftUp\TaskBoxxBundle\Entity;
 
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 
-/** 
+/**
  * @orm:Entity() 
  * @orm:Table(name="tb_user") 
- */ 
-class User implements UserInterface 
-{ 
-    /** 
+ */
+class User implements UserInterface
+{
+
+    /**
      * @orm:Id 
      * @orm:Column(type="integer") 
      * @orm:GeneratedValue(strategy="IDENTITY") 
-     */ 
-    protected $id; 
-    
-    /** 
+     */
+    protected $id;
+    /**
      * @orm:Column(type="string", length="32", unique=true) 
-     */ 
-    protected $username; 
-    
-    /** 
+     */
+    protected $username;
+    /**
      * @orm:Column(type="string", length="255", unique=true) 
-     */ 
-    protected $email; 
-    
-    /** 
+     */
+    protected $email;
+    /**
      * @orm:Column(type="string", length="128") 
-     */ 
-    protected $password; 
-    
+     */
+    protected $password;
     /**
      * @orm:OneToMany(targetEntity="BacklogItem", mappedBy="assignedUser")
      * @var ArrayCollection $backlogItems 
      */
     protected $backlogItems;
-    
     /**
      * @orm:ManyToMany(targetEntity="Role")
      * @orm:JoinTable(name="user_role",
@@ -48,8 +45,12 @@ class User implements UserInterface
      * @var ArrayCollection $userRoles
      */
     protected $userRoles;
-    
-    
+    /**
+     * @orm:Column(type="string", length="5")
+     * @var String
+     */
+    protected $locale;
+
     /**
      * Constructs a new instance of User
      */
@@ -57,42 +58,47 @@ class User implements UserInterface
     {
         $this->userRoles = new ArrayCollection();
     }
-    
-    
-    public function getId() 
-    { 
-        return $this->id; 
-    } 
-    public function setId($id) 
-    { 
-        $this->id = $id; 
-    } 
-    public function getUsername() 
-    { 
-        return $this->username; 
-    } 
-    public function setUsername($username) 
-    { 
-        $this->username = $username; 
-    } 
-    public function getPassword() 
-    { 
-        return $this->password; 
-    } 
-    public function setPassword($password) 
-    { 
-        $this->password = $password; 
-    } 
-    
-    public function setEmail($email) 
-    { 
-        $this->email = $email; 
-    } 
-    public function getEmail() 
-    { 
-        return $this->email; 
-    } 
-    
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
     /**
      * Gets the user roles.
      *
@@ -102,15 +108,15 @@ class User implements UserInterface
     {
         return $this->userRoles;
     }
-    
-    /** 
+
+    /**
      * Implementing the UserInterface interface 
-     */ 
-    public function __toString() 
-    { 
-        return $this->getUsername(); 
-    } 
-    
+     */
+    public function __toString()
+    {
+        return $this->getUsername();
+    }
+
     /**
      * Gets an array of roles.
      * 
@@ -120,31 +126,42 @@ class User implements UserInterface
     {
         return $this->getUserRoles()->toArray();
     }
-    
-    public function eraseCredentials() 
-    { 
-    } 
-    
-    public function getSalt() 
-    { 
-        return $this->getId(); 
-    } 
-    
-    /** 
+
+    public function eraseCredentials()
+    {
+        
+    }
+
+    public function getSalt()
+    {
+        return $this->getId();
+    }
+
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
      * equals. 
      * 
      * @param UserInterface $account 
      * @return bool 
-     */ 
-    public function equals(UserInterface $account) 
-    { 
-        if ($account->getUsername() != $this->getUsername()) { 
-            return false; 
-        } 
-        if ($account->getEmail() != $this->getEmail()) { 
-            return false; 
-        } 
-        return true; 
-    } 
-    
+     */
+    public function equals(UserInterface $account)
+    {
+        if ($account->getUsername() != $this->getUsername()) {
+            return false;
+        }
+        if ($account->getEmail() != $this->getEmail()) {
+            return false;
+        }
+        return true;
+    }
+
 }
